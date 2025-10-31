d10550f825475fb887de948fdc710722b72173ab
Nadila Rizkika <nadilarizkika@gmail.com>
2025-10-31 17:12:14 +0700

    soal nomor 3

--- resources/views/produk.blade.php
+++ resources/views/produk.blade.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman Home</title>
+    <title>Halaman Produk</title>
 
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <body>
 
@@ -14,10 +15,8 @@
     <nav class="navbar navbar-light bg-light">
         <div class="container-fluid d-flex justify-content-between">
 
-            <!-- kiri -->
             <span class="navbar-brand mb-0 h1">UTS Laravel</span>
 
-            <!-- kanan (tombol sidebar) -->
             <button class="navbar-toggler border rounded" type="button"
                 data-bs-toggle="offcanvas" data-bs-target="#sideBarCanvas">
                 <span class="navbar-toggler-icon"></span>
@@ -25,7 +24,7 @@
         </div>
     </nav>
 
-    <!-- Sidebar Offcanvas (kosong) -->
+    <!-- Sidebar -->
     <div class="offcanvas offcanvas-end" tabindex="-1" id="sideBarCanvas">
         <div class="offcanvas-header">
             <h5 class="offcanvas-title">UTS Laravel</h5>
@@ -33,12 +32,10 @@
         </div>
 
         <div class="offcanvas-body">
-            <!-- Menu -->
             <a href="/" class="d-block text-decoration-none text-secondary {{ request()->path() === '/' ? 'text-dark' : '' }}">Home</a>
             
             <a href="/produk" class="d-block text-decoration-none text-secondary mt-2 {{ request()->path() === 'produk' ? 'text-dark' : '' }}">Produk</a>
 
-            <!-- Search -->
             <form class="d-flex mt-3">
                 <input class="form-control me-2" type="search" placeholder="Search">
                 <button class="btn btn-outline-success" type="button">Search</button>
@@ -46,9 +43,41 @@
         </div>
     </div>
 
-    <!-- Konten -->
-    <div class="container-fluid bg-light py-0">
-        <h1 style="margin-left: 120px; margin-top: 0px">Halaman Home</h1>
+    <!-- Content -->
+    <div class="container">
+
+        <!-- JUDUL + TOMBOL -->
+        <div class="d-flex justify-content-between align-items-center" style="max-width: 1020px; margin: 0 auto;">
+            <h1 class="mb-0">Halaman Produk</h1>
+            <a href="/tambah-produk" class="btn btn-primary">Tambah Produk</a>
+        </div>
+
+        <!-- TABEL -->
+        <div class="table-responsive">
+            <table class="table table-striped table-primary table-sm mt-2"
+                   style="max-width: 1020px; margin: 0 auto;">
+                <thead>
+                    <tr>
+                        <th>Kode Produk</th>
+                        <th>Nama Produk</th>
+                        <th>Jenis Produk</th>
+                        <th>Harga</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @for ($i = 0; $i < count($produk); $i++)
+                        <tr>
+                            <td>{{ $produk[$i]['kode'] }}</td>
+                            <td>{{ $produk[$i]['nama'] }}</td>
+                            <td>{{ $produk[$i]['jenis'] }}</td>
+                            <td>{{ $produk[$i]['harga'] }}</td>
+                        </tr>
+                    @endfor
+                </tbody>
+            </table>
+        </div>
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/produk', function () {
+
+    $produk = [
+        ["kode" => "BRG001", "nama" => "Pena", "jenis" => "Alat tulis", "harga" => 20000],
+        ["kode" => "BRG002", "nama" => "Buku", "jenis" => "Alat tulis", "harga" => 15000],
+    ];
+
+    return view('produk', ['produk' => $produk]);
+});
